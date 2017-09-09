e5459a651faf648de7bb347acdf10dc3cb05cca9
Iris Schaffer <irisschaffer@gmx.net>
2017-09-10 00:16:40 +0200

    Enables markdown in footer

--- src/data/content/Footer.js
+++ src/data/content/Footer.js
@@ -1,7 +1,20 @@
 import { Record } from 'immutable'
+import { markdown } from 'utils/string'
 
 const FooterRecord = new Record({
 	text : ''
 }, 'footer')
 
-export default FooterRecord
+
+export default class Footer extends FooterRecord {
+	constructor(data = {}, name) {
+		if (data instanceof Footer) {
+			super(data, name)
+		} else {
+			super({
+				...data,
+				text : markdown(data.text || '')
+			}, name)
+		}
+	}
+}
