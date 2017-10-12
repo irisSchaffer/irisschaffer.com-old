ffd50916374a280e5475e09e5acf17b5b5434ee7
Iris Schaffer <irisschaffer@gmx.net>
2017-10-12 22:54:38 +0200

    Now delivers gzipped bundle

--- server/production.js
+++ server/production.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const expressStaticGzip = require('express-static-gzip')
 
 const render = require('../build/render')
 const assets = require('../build/assets.json')
@@ -6,7 +6,7 @@ const chunks = require('../build/chunks.json')
 const icons = require('../build/icons.json')
 
 module.exports = app => {
-	app.use(express.static('./build'))
+	app.use('/', expressStaticGzip('./build'))
 	app.use('*', render.default({ assets, chunks, icons }))
 
 	return app
